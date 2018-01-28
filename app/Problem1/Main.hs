0c9a88573d3de279372b7c422a2f59023a3b3139
Justin Christensen <wroathe@gmail.com>
2018-01-28 16:35:33 -0600

    solved problem 1

--- app/Problem1/Main.hs
+++ app/Problem1/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-import Lib
+import Lib (sumMultOf3And5)
 
 main :: IO ()
-main = someFunc
+main = 
+    let prependAnswer = (++) "The sum of multiples of 3 and 5 between 3 and 999 is:\n"
+    in putStrLn $ prependAnswer $ show $ sumMultOf3And5 [3..999]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
 module Lib
-    ( someFunc
+    ( sumMultOf3And5
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+isMultOf3And5 :: (Integral i) => i -> Bool
+isMultOf3And5 i = 
+    let eq0 = (==) 0
+        modi = mod i
+    in (eq0 $ modi 3) || (eq0 $ modi 5)
+
+sumMultOf3And5 :: (Integral a) => [a] -> a
+sumMultOf3And5 xs = sum $ filter isMultOf3And5 xs
