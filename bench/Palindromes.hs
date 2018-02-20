1601f310757e3eab678976e20e1514e659296dd8
Justin Christensen <wroathe@gmail.com>
2018-02-19 18:07:14 -0600

    fix benchmark

--- bench/Palindromes.hs
+++ bench/Palindromes.hs
@@ -3,12 +3,8 @@ import Euler
 
 main = 
     let sumNPalindromes n = sum $ take n $ palindromes
-        sumNPalindromesS n = sum $ take n $ palindromesS
     in defaultMain [
         bgroup "palindromes" [ 
             bench "5000" $ whnf sumNPalindromes 5000
-        ],
-        bgroup "palindromesS" [ 
-            bench "5000" $ whnf sumNPalindromesS 5000
         ]
     ]
